0e1393efa6398e4699b9cf1267b2d9a0a7940fcc
Fetra Randriannirina <fetrarandriannirina@gmail.com>
2026-03-14 22:34:26 +0300

    C# Bootcamp first project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿var nom = "Fetra";
+var age = 20;
+var pays = "Madagascar";
+var objectif1 = "Backend Developer";
+var objectif2 = "Game Developer";
+
+Console.WriteLine("======================================================");
+Console.WriteLine("C# Bootcamp\n");
+
+Console.WriteLine($"Nom : {nom}");
+Console.WriteLine($"Age : {age}");
+Console.WriteLine($"Pays : {pays}");
+Console.WriteLine($"Objectif 1 : {objectif1}");
+Console.WriteLine($"Objectif 2 : {objectif2}");
+
+Console.WriteLine("======================================================");
